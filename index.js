46df5f09fc44ce68a0b4a9629214148ad24b2b92
OJAS SRIVASTAVA <ojas.srivastava2003@gmail.com>
2023-04-17 17:52:36 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+let clip=document.querySelector(".video")
+clip.addEventListener("mouseover",function(e){
+  clip.play();
+})
+clip.addEventListener("mouseout",function(e){
+  clip.pause();
+})
+
+
+
+
+function setNewImage()
+{
+   document.getElementById("pic1").src="images/pic 21b.jpeg";
+}
+function setOldImage()
+{
+    document.getElementById("pic1").src="images/pic 21a.jpeg";
+}
+function setNewImage1()
+{
+   document.getElementById("pic2").src="images/pic 22b.jpeg";
+}
+function setOldImage1()
+{
+    document.getElementById("pic2").src="images/pic 22a.jpeg";
+}
+function setNewImage2()
+{
+   document.getElementById("pic3").src="images/pic 23b.jpeg";
+}
+function setOldImage2()
+{
+    document.getElementById("pic3").src="images/pic 23a.jpeg";
+}
+function setNewImage3()
+{
+   document.getElementById("pic4").src="images/pic 24b.jpeg";
+}
+function setOldImage3()
+{
+    document.getElementById("pic4").src="images/pic 24a.jpeg";
+}
